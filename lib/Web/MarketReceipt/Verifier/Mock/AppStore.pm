09e7ddb91d40890ed24c152fd874616958bb6e5b
mackee <macopy123@gmail.com>
2018-03-14 14:29:49 +0900

    fix regexp: from https://github.com/kayac/p5-Web-MarketReceipt/pull/5#discussion_r174350861

--- lib/Web/MarketReceipt/Verifier/Mock/AppStore.pm
+++ lib/Web/MarketReceipt/Verifier/Mock/AppStore.pm
@@ -78,7 +78,7 @@ sub _parse_oldstlye_plist {
 
     my $data = decode_base64($text);
     my ($properties_str) = $data =~ /{(.*)}/s;
-    my %kv = $properties_str =~ /\s*"(\S+)"\s*=\s*"(.+)";/g;
+    my %kv = $properties_str =~ /\s*"(.*?)"\s*=\s*"(.*?)"\s*;/msg;
 
     return \%kv;
 }
